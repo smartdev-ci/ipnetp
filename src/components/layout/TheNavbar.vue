86c6e26fc01911c1bce48f6b854860143a4594c2
Franck Koffi <franck.koffi@epitech.eu>
2025-06-11 16:15:19 +0000

    [Update] director page co:mpleted

--- src/components/layout/TheNavbar.vue
+++ src/components/layout/TheNavbar.vue
@@ -301,7 +301,6 @@ li {
   list-style: none;
   margin: 0;
   padding: 0;
-  /* overflow: scroll; */
 }
 
 
--- src/views/Direction.vue
+++ src/views/Direction.vue
@@ -1,46 +1,30 @@
 <template>
-<HeroSection background="https://scontent.fabj7-1.fna.fbcdn.net/v/t39.30808-6/495367572_122206707464171003_906942137193410451_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=127cfc&_nc_eui2=AeHGem9psvlXeRUVw-nCMstkrRVQIYMWKaetFVAhgxYpp1bx3OY9BhpxvQHqPoSPR2CF4u11p4uUGtoT5nel6Vgi&_nc_ohc=Xt8IBYYjW5wQ7kNvwEvbuGe&_nc_oc=AdnhkT6JyIQ4aTo_5We0l0HM16znBzQUhWRluJ9RCRZZ9mzJ9uDBvSRnnJCZeTCgauI&_nc_zt=23&_nc_ht=scontent.fabj7-1.fna&_nc_gid=D7Pnnez1G5MCixLZx04mJg&oh=00_AfLtiDtZli-1ZACve9magPm4D7bh5xDnfWzUsXQl4sjnIQ&oe=6820F816">
-  <h1 class="hero-title">LES MOTS DE LA DIRECTION</h1>
-</HeroSection>
   <div class="page-container">
-    <!-- Header Section -->
-    <section class="header-direction">
-      <!-- <h1>LES MOTS DE LA DIRECTION</h1> -->
-    </section>
-
-    <!-- Intro Section
-      <section class="intro">
-        <h2>Bienvenue à l'Ensemble Montplaisir !</h2>
-        <p>
-          Montplaisir est un ensemble scolaire situé à Valence dans la Drôme. Il se compose de 4 unités pédagogiques réparties sur deux sites :
-        </p>
-        <ul>
-          <li>Le premier, situé dans les bâtiments historiques...</li>
-          <li>Le second, situé dans la zone industrielle...</li>
-        </ul>
-      </section>
-       -->
-    <!-- First Content Section -->
+    <h1>Mot de la directrice</h1>
     <section class="content">
-      <div class="image-block">
+      <div class="image-block dg-img">
         <img src="https://ipnetp.ci/wp-content/uploads/2023/12/DG2.png" />
       </div>
       <div class="text-block">
         <h2>Intégrer l'IPNETP</h2>
         <p>
-          Chers étudiants, chers partenaires, chers visiteurs, <br>
-          C’est avec une immense joie que je vous souhaite la bienvenue sur le site officiel de l’Institut Pédagogique
-          National de l’Enseignement Technique et Professionnel (IPNETP).
-          Depuis sa création, notre établissement s’engage à former des professionnels compétents, passionnés et prêts à
-          relever les défis du monde de l’enseignement technique et de la formation professionnelle. Forts d’un corps
-          enseignant qualifié, de programmes adaptés aux réalités du marché et d’un environnement propice à
-          l’apprentissage, nous œuvrons chaque jour à l’excellence académique et à l’innovation pédagogique.
-          Que vous soyez un étudiant en quête de formation, un professionnel désireux d’enrichir ses compétences, ou un
-          partenaire souhaitant collaborer avec nous, sachez que l’IPNETP vous ouvre ses portes avec enthousiasme et
-          engagement.
-          Nous vous invitons à explorer notre site pour découvrir nos formations, nos actualités et nos opportunités.
-          Ensemble, bâtissons l’avenir de l’éducation et de la formation professionnelle !
-          Bienvenue à l’IPNETP !
+          Chers étudiants, chers partenaires, chers visiteurs, <br />
+          C'est avec une immense joie que je vous souhaite la bienvenue sur le
+          site officiel de l'Institut Pédagogique National de l'Enseignement
+          Technique et Professionnel (IPNETP). Depuis sa création, notre
+          établissement s'engage à former des professionnels compétents,
+          passionnés et prêts à relever les défis du monde de l'enseignement
+          technique et de la formation professionnelle. Forts d'un corps
+          enseignant qualifié, de programmes adaptés aux réalités du marché et
+          d'un environnement propice à l'apprentissage, nous œuvrons chaque jour
+          à l'excellence académique et à l'innovation pédagogique. Que vous
+          soyez un étudiant en quête de formation, un professionnel désireux
+          d'enrichir ses compétences, ou un partenaire souhaitant collaborer
+          avec nous, sachez que l'IPNETP vous ouvre ses portes avec enthousiasme
+          et engagement. Nous vous invitons à explorer notre site pour découvrir
+          nos formations, nos actualités et nos opportunités. Ensemble,
+          bâtissons l'avenir de l'éducation et de la formation professionnelle !
+          Bienvenue à l'IPNETP !
         </p>
       </div>
     </section>
@@ -48,35 +32,51 @@
     <!-- Second Content Section (Reversed) -->
     <section class="content reverse">
       <div class="image-block">
-        <img src="https://ipnetp.ci/wp-content/uploads/2023/12/2.jpg" alt="Illustration" />
+        <img
+          src="https://ipnetp.ci/wp-content/uploads/2023/12/2.jpg"
+          alt="Illustration"
+        />
       </div>
       <div class="text-block">
         <h2>Choisir une formation parmi des parcours variés...</h2>
         <p>
-          Depuis sa création, l'IPNETP est une institution dédiée à la formation des futurs formateurs de l'enseignement
-          technique. Sa mission principale consiste à transmettre des connaissances pédagogiques et techniques à travers
-          des programmes complets alliant théorie et pratique. L'École actualise régulièrement ses contenus pour
-          s'adapter aux évolutions technologiques et pédagogiques ainsi qu'aux exigences du marché du travail. En créant
-          un environnement d'apprentissage dynamique et collaboratif, elle favorise les échanges entre formateurs en
-          formation. Son rôle est donc essentiel pour préparer une nouvelle génération de professionnels qualifiés,
-          contribuant ainsi à l'amélioration de l'éducation professionnelle.
+          Depuis sa création, l'IPNETP est une institution dédiée à la formation
+          des futurs formateurs de l'enseignement technique. Sa mission
+          principale consiste à transmettre des connaissances pédagogiques et
+          techniques à travers des programmes complets alliant théorie et
+          pratique. L'École actualise régulièrement ses contenus pour s'adapter
+          aux évolutions technologiques et pédagogiques ainsi qu'aux exigences
+          du marché du travail. En créant un environnement d'apprentissage
+          dynamique et collaboratif, elle favorise les échanges entre formateurs
+          en formation. Son rôle est donc essentiel pour préparer une nouvelle
+          génération de professionnels qualifiés, contribuant ainsi à
+          l'amélioration de l'éducation professionnelle.
         </p>
       </div>
     </section>
   </div>
 </template>
 
-<script setup lang="ts">
-  import HeroSection from "../components/home/HeroSection.vue";
-</script>
+<script setup lang="ts"></script>
 
 <style>
+h1 {
+  border-bottom: solid 2px;
+  width: fit-content;
+  margin: auto;
+}
+
 .page-container {
-  /* padding-top: 10%; */
+  padding-top: 190px;
   width: 80%;
   margin: 0 auto;
   font-family: sans-serif;
   color: #12274a;
+  position: relative;
+  height: fit-content;
+  display: flex;
+  justify-content: start;
+  flex-direction: column;
 }
 
 .header-direction {
@@ -114,28 +114,29 @@
 .content {
   display: flex;
   align-items: center;
+  justify-content: center;
   margin: 20px 0;
   gap: 50px;
-
-  .text-block {
-    box-shadow: 0 0 35px 0 rgba(0, 0, 0, .09);
-    border-radius: 20px;
-  }
+  padding-top: 140px;
 }
 
+.text-block {
+  border-radius: 20px;
+  box-shadow: 0 0 35px 0 rgba(0, 0, 0, 0.09);
+}
 .content.reverse {
   flex-direction: row-reverse;
+  margin-bottom: 100px;
   gap: 50px;
 }
 
 .text-block {
-  width: 50%;
+  width: 500px;
   padding: 20px;
 
   p {
     text-align: justify;
   }
-
 }
 
 .text-block h2 {
@@ -149,12 +150,41 @@
 }
 
 .image-block {
-  width: 50%;
+  width: 600px;
+  height: 370px;
+  box-shadow: 0 0 35px 0 rgba(0, 0, 0, 0.09);
+}
+
+.image-block {
+  border-radius: 8px;
 }
 
 .image-block img {
   width: 100%;
   border-radius: 8px;
-  height: 330px;
+  height: 100%;
+  object-fit: cover;
+}
+
+/* Responsive */
+@media (max-width: 1200px) {
+  .content,
+  .content.reverse {
+    flex-direction: column;
+  }
+
+  .text-block {
+    width: 100%;
+    padding: 20px;
+  }
+}
+
+@media (max-width: 668px) {
+  .image-block {
+    width: 88vw;
+    height: 50vw;
+    min-width: 300px;
+    min-height: 100px;
+  }
 }
 </style>
