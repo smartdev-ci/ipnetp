953f4ff7f8f187f03c71e17aa04dfe078d43c53f
Franck Koffi <franck.koffi@epitech.eu>
2025-06-13 09:09:56 +0000

    [Update] hero image size updated

--- src/components/home/HeroSection.vue
+++ src/components/home/HeroSection.vue
@@ -14,17 +14,30 @@ defineProps<{
 
 <style scoped>
 .hero-section {
-  margin-top: 60px;
-  height: 90vh;
+  margin-top: 85px;
+  height: 44vw;
   display: flex;
   align-items: center;
-  background-size: cover;
-  background-position: center;
+  background-size: 100%;
+  background-repeat: no-repeat;
+  overflow: hidden;
   position: relative;
   color: white;
   padding-top: 60px;
   transition: background-image 0.5s ease-in-out;
 }
+
+.hero-section::after{
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #ffffff;
+  height: 1px;
+  
+}
+
 .hero-content {
   max-width: 600px;
   margin-left: 10%;
