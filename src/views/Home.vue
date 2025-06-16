0e016d6f2e570d06ab162381335368a71a6183ed
Franck Koffi <franck.koffi@epitech.eu>
2025-06-16 12:04:25 +0000

    [Add] concours 2025 button add on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,13 @@
 <!-- views/Home.vue -->
 <template>
   <main>
-    <HeroSection background="https://ipnetp.ci/wp-content/uploads/2025/04/1-scaled.png"/>
+    <HeroSection
+      background="https://ipnetp.ci/wp-content/uploads/2025/04/1-scaled.png"
+    />
     <FlashSection />
+    <section class="concours225">
+      <a href="https://sigec.ipnetp.cloud/auth/signup" target="_blank"></a>
+    </section>
     <WhyEducat />
     <NewsSection />
     <LibrarySection />
@@ -11,10 +16,33 @@
 </template>
 <script setup lang="ts">
 import HeroSection from "../components/home/HeroSection.vue";
-import FlashSection from '../components/home/FlashSection.vue';
-import WhyEducat from '../components/home/WhyEducat.vue';
-import NewsSection from '../components/news/NewsSection.vue';
-import LibrarySection from '../components/products/LibrarySection.vue';
-import EventsSection from '../components/events/EventsSection.vue';
-
+import FlashSection from "../components/home/FlashSection.vue";
+import WhyEducat from "../components/home/WhyEducat.vue";
+import NewsSection from "../components/news/NewsSection.vue";
+import LibrarySection from "../components/products/LibrarySection.vue";
+import EventsSection from "../components/events/EventsSection.vue";
 </script>
+
+<style scoped>
+.concours225 {
+  height: 7vw;
+  width: 85vw;
+  margin: auto;
+  border: 1px solid #000;
+  background: url("https://ipnetp.ci/wp-content/uploads/2025/05/21-2048x157.png");
+  background-size: cover;
+  margin-bottom: 45px;
+}
+
+.concours225 a {
+  display: block;
+  height: 100%;
+  width: 100%;
+}
+
+@media (max-width: 505px) {
+  .concours225 {
+
+  }
+}
+</style>
